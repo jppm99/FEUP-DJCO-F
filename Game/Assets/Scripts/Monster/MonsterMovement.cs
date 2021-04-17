a80c65d2361a38125a2bfee2f6ff08b145aceeee
André Gomes <andremsgomes@gmail.com>
2021-04-17 22:24:32 +0100

    Changed monster prefab

---
+++ Game/Assets/Scripts/Monster/MonsterMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // FixedUpdate is called once every physic update
+    private void FixedUpdate()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
