bc65c67ba9c710e537f189815b2525537fb8c831
André Gomes <andremsgomes@gmail.com>
2021-04-18 17:32:36 +0100

    start function and monster doesn't fall

--- Game/Assets/Scripts/Monster/MonsterMovement.cs
+++ Game/Assets/Scripts/Monster/MonsterMovement.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // FixedUpdate is called once every physic update
