c5ac61725ea6524463d29233ac3d19bada191be0
André Gomes <andremsgomes@gmail.com>
2021-04-19 02:41:05 +0100

    Added rotation

--- Game/Assets/Scripts/Monster/MonsterMovement.cs
+++ Game/Assets/Scripts/Monster/MonsterMovement.cs
@@ -4,6 +4,8 @@ public class MonsterMovement : MonoBehaviour
 {
     private Rigidbody rb;
 
+    private int angle;
+
     public float speed;
 
 
@@ -11,17 +13,27 @@ public class MonsterMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        angle = 0;
     }
 
     // FixedUpdate is called once every physic update
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(speed, rb.velocity.y, 0);
+
+        Rotate();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    // Update angle
+    private void Rotate()
+    {
+        Quaternion target = Quaternion.Euler(0, angle, 0);
+        rb.rotation = Quaternion.Slerp(rb.rotation, target, Time.deltaTime);
     }
 }
