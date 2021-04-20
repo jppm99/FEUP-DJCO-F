a2f1fcfc61a9d018e699bb841d430938cca5b772
André Gomes <andremsgomes@gmail.com>
2021-04-20 18:40:58 +0100

    Added angle update

--- Game/Assets/Scripts/Monster/MonsterMovement.cs
+++ Game/Assets/Scripts/Monster/MonsterMovement.cs
@@ -5,6 +5,8 @@ public class MonsterMovement : MonoBehaviour
     private Rigidbody rb;
 
     private float angle;
+    private int updates;
+    private int target_updates;
 
     public float speed;
     public float rotation_speed;
@@ -15,6 +17,8 @@ public class MonsterMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         angle = 0;
+        updates = 0;
+        target_updates = 1000;
     }
 
     // FixedUpdate is called once every physic update
@@ -24,8 +28,14 @@ public class MonsterMovement : MonoBehaviour
 
         if (target != rb.rotation)
             Rotate(target);
-        else 
-            rb.velocity = new Vector3(speed, rb.velocity.y, 0);
+        else {
+            if (updates == target_updates)
+                UpdateAngle();
+            else {
+                rb.velocity = new Vector3(speed, rb.velocity.y, 0);
+                updates++;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -38,5 +48,14 @@ public class MonsterMovement : MonoBehaviour
     private void Rotate(Quaternion target)
     {
         rb.rotation = Quaternion.Slerp(rb.rotation, target, Time.deltaTime * rotation_speed);
+    }
+
+    // Update angle
+    private void UpdateAngle()
+    {
+        float delta = Random.Range(-180.0f, 180.0f);
+        
+        angle = angle + delta;
+        updates = 0;
     }
 }
