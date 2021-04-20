f4ce6198b8f0eeb6f80cfa352505c0e445c123d8
Duarte Carvalho <up201503661@fe.up.pt>
2021-04-20 12:39:02 +0100

    Raycast done and MonsterLife script created

--- Game/Assets/Scripts/Player/PlayerAttack.cs
+++ Game/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private const int LEFT_MOUSE_CLICK = 0;
+    [SerializeField] private int attackDistance;
+    [SerializeField] private int attackDamage;
+
+    GameObject playerCamera;
+
     void Start()
     {
-        
+        playerCamera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(LEFT_MOUSE_CLICK))
+        {
+            RaycastHit ray;
+
+            /* In the future a layermask must be added as a parameter to this function */
+            if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.TransformDirection(Vector3.forward), out ray, attackDistance))
+            {
+                string objectTag = ray.transform.tag;
+
+                if(objectTag.Equals("monster"))
+                {
+                    MonsterLife enemy = ray.transform.GetComponent<MonsterLife>();
+
+                    enemy.damage(attackDamage);
+                }
+            }
+        }
     }
 }
