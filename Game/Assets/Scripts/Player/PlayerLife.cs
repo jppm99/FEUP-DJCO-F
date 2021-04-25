ee42fab5e89eac190404621edb2a41b0ed7d95fc
TigasMiller09 <48095097+TigasMiller09@users.noreply.github.com>
2021-04-25 16:37:29 +0100

    Add food bar

--- Game/Assets/Scripts/Player/PlayerLife.cs
+++ Game/Assets/Scripts/Player/PlayerLife.cs
@@ -8,17 +8,32 @@ public class PlayerLife : MonoBehaviour
     private Image healthImage;
     private Text healthText;
 
+    private Image foodImage;
+    private Text foodText;
+
     [SerializeField]
     private float maxHealth;
     private float health;
+    [SerializeField]
+    private float healthLossDelay;
+
+    [SerializeField]
+    private float maxFood;
+    private float food;
+    [SerializeField]
+    private float foodLossDelay;
+    private float nextActionTime = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         healthImage = GameObject.Find("HealthImage").GetComponent<Image>();
         healthText = GameObject.Find("HealthText").GetComponent<Text>();
+        foodImage = GameObject.Find("FoodImage").GetComponent<Image>();
+        foodText = GameObject.Find("FoodText").GetComponent<Text>();
 
         health = maxHealth;
+        food = maxFood;
     }
 
     // Update is called once per frame
@@ -26,10 +41,28 @@ public class PlayerLife : MonoBehaviour
     {
         healthImage.fillAmount = health / maxHealth;
         healthText.text = health.ToString() + " / " + maxHealth.ToString();
+
+        foodImage.fillAmount = food / maxFood;
+        foodText.text = food.ToString() + " / " + maxFood.ToString();
+
+        decreaseElementOverTime(ref food, foodLossDelay);
+
+        if (food == 0)
+            decreaseElementOverTime(ref health, healthLossDelay);
+
     }
 
     public void decreaseHealth(float damage)
     {
         health -= damage;
+    }
+
+    private void decreaseElementOverTime(ref float element, float delay)
+    {
+        if (Time.time > nextActionTime && element >= 1)
+        {
+            element -= 1f;
+            nextActionTime += delay;
+        }
     }
 }
