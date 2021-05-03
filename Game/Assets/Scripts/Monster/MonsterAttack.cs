a7cdf67b83184703dd6e09d6b765ee864de9c3cf
duartencar <up201503661@fe.up.pt>
2021-05-03 13:15:25 +0100

    Monster atack is more efficient

--- Game/Assets/Scripts/Monster/MonsterAttack.cs
+++ Game/Assets/Scripts/Monster/MonsterAttack.cs
@@ -8,36 +8,44 @@ public class MonsterAttack : MonoBehaviour
     public float atackRadius;
     public float atackInterval;
     public int atackDamage;
+    private float timeSinceLastAtack;
 
     private GameObject player;
     private Transform playerTransform;
-
-    private IEnumerator coroutine;
 
     void Start()
     {
         player = GameObject.Find("Player");
         playerTransform = player.transform;
-
-        coroutine = atackPlayerIfPossible(atackInterval);
-
-        StartCoroutine(coroutine);
+        timeSinceLastAtack = 1f;
     }
 
-    private IEnumerator atackPlayerIfPossible(float waitTime)
+    /**
+     * Every physics update, increments a counter that starts with the atack interval value,
+     * this allows the monster to atack as soon as it gets near enough to the player. When 
+     * the monster atacks, the counter resets and it only atacks after the defined
+     * atackInterval
+     */
+    void FixedUpdate()
     {
-        while (true)
-        {
-            yield return new WaitForSeconds(waitTime);
+        timeSinceLastAtack = Math.Min(atackInterval, timeSinceLastAtack + Time.fixedDeltaTime);
 
+        if(timeSinceLastAtack >= atackInterval)
+        {
             float dist = Vector3.Distance(playerTransform.position, transform.position);
 
             if (dist < atackRadius)
             {
-                PlayerLife life = player.GetComponent<PlayerLife>();
-
-                life.decreaseHealth(atackDamage);
-            } 
+                atackPlayer();
+                timeSinceLastAtack = 0;
+            }
         }
+    }
+
+    void atackPlayer()
+    {
+        PlayerLife life = player.GetComponent<PlayerLife>();
+
+        life.decreaseHealth(atackDamage);
     }
 }
