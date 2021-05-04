f27dcd9d32e1af5939582e43fb400eb3da675235
TigasMiller09 <48095097+TigasMiller09@users.noreply.github.com>
2021-05-04 16:14:53 +0100

    Basic animations done

--- Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -16,17 +16,31 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded;
     public LayerMask groundMask;
 
+    Animator playerAnimator;
+    private float playerSpeed = 0;
+    Vector3 lastPosition = Vector3.zero;
+
+    private bool isRunning = false;
+    private bool isWalking = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = gameObject.GetComponent<CharacterController>();
+        playerAnimator = GameObject.Find("PlayerBody").GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
         groundCheck = GameObject.Find("Ground Check").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerSpeed = (new Vector3(transform.position.x, 0, transform.position.z) - new Vector3 (lastPosition.x, 0, lastPosition.z)).magnitude;
+        lastPosition = transform.position;
+
+        //Debug.Log(playerSpeed);
+
+
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundMask);
 
         float x = Input.GetAxis("Horizontal");
@@ -37,13 +51,32 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = Mathf.Sqrt(jumpHeigh * -3f * this.gravity);
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) && canRun)
-            speed = 15f;
-        else if (Input.GetKey(KeyCode.LeftControl))
+
+        if (Input.GetKey(KeyCode.LeftShift) && canRun && (x != 0 || z != 0))
+        {
+            speed = 8f;
+            playerAnimator.SetBool("isRunning", true);
+            playerAnimator.SetBool("isWalking", false);
+        }
+        else if (Input.GetKey(KeyCode.LeftControl) && (x != 0 || z != 0))
+        {
+            speed = 2.5f;
+            playerAnimator.SetBool("isRunning", false);
+            playerAnimator.SetBool("isWalking", true);
+        }
+        else if((x != 0 || z != 0))
+        {
             speed = 5f;
+            playerAnimator.SetBool("isRunning", false);
+            playerAnimator.SetBool("isWalking", true);
+        }
         else
-            speed = 10f;
-            
+        {
+            speed = 0f;
+            playerAnimator.SetBool("isRunning", false);
+            playerAnimator.SetBool("isWalking", false);
+        }
+
 
         Vector3 movement = transform.right * x + transform.forward * z;
 
@@ -56,6 +89,9 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(movement * speed * Time.deltaTime);
         controller.Move(velocity * Time.deltaTime);
+
+
+        //playerAnimator.SetFloat("Velocity", playerSpeed / 1f);
     }
 
     public void setNotBeingAbleToRun(bool canRun)
