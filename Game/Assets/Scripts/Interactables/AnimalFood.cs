428e9869390050ecbc1bb73d11cded4909a9ceda
João Mota <47133204+jppm99@users.noreply.github.com>
2021-05-07 14:44:07 +0100

    interactable food script

---
+++ Game/Assets/Scripts/Interactables/AnimalFood.cs
@@ -0,0 +1,22 @@
+public class AnimalFood : Interactable
+{
+    protected override void Action()
+    {
+        RuntimeStuff.GetSingleton<Inventory>().AddFood();
+        Destroy(this.gameObject);
+    }
+
+    protected override void Start()
+    {
+        this.floatingText = "Press F to grab food";
+        base.Start();
+
+        // Disable interactions: animal only turns into food when dead
+        this.Disable();
+    }
+
+    public void EnableInteraction()
+    {
+        this.Enable();
+    }
+}
