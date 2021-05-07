49906395e67f3cbac594c86b8ce609abd9843bba
João Mota <47133204+jppm99@users.noreply.github.com>
2021-05-07 14:43:04 +0100

    Add animal life script

---
+++ Game/Assets/Scripts/Animal/AnimalLife.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class AnimalLife : MonoBehaviour
+{
+    [SerializeField] private int maxHealth, currentHealth, secondsUntilBodyDisapears;
+    private AnimalFood interactableScript;
+
+    private void Start() {
+        this.interactableScript = this.GetComponent<AnimalFood>();
+    }
+
+    public void damage(int damage)
+    {
+        Debug.Log(currentHealth);
+
+        // Hitpoints cannnot go below 0
+        currentHealth = Math.Max(currentHealth - damage, 0);
+
+        if(currentHealth <= 0)
+        {
+            StartCoroutine(die());
+        }
+    }
+
+    private IEnumerator die()
+    {
+        this.interactableScript.EnableInteraction();
+
+        yield return new WaitForSeconds(this.secondsUntilBodyDisapears);
+
+        Destroy(this.gameObject);
+    }
+}
