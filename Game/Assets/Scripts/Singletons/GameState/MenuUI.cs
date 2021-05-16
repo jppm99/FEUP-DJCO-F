874c31a12d1170d7d74d7d99c815b327d418bbce
Duarte Carvalho <up201503661@fe.up.pt>
2021-05-16 18:44:00 +0100

    initial Logic

--- Game/Assets/Scripts/Singletons/GameState/MenuUI.cs
+++ Game/Assets/Scripts/Singletons/GameState/MenuUI.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private enum MenuContext {
+        StartMenu,
+        GameMenu,
+        EndMenu
+    }
+
+    private MenuContext context;
+    private bool menuIsEnabled;
+
     void Start()
     {
-        
+        context = MenuContext.StartMenu;
+        menuIsEnabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && context == MenuContext.GameMenu)
+        {
+            menuIsEnabled = !menuIsEnabled;
+        }
+    }
+
+    public void gameStarted()
+    {
+        context = MenuContext.GameMenu;
+    }
+
+    public void gameEnded()
+    {
+        context = MenuContext.EndMenu;
     }
 }
